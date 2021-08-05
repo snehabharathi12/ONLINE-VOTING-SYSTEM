4d7a4d4e47395c80f013eefcb4f3cbb0420bf262
snehabharathi12 <88458262+snehabharathi12@users.noreply.github.com>
2021-08-05 14:21:38 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>dashboard</title>
+</head>
+<style type="text/css">
+	body{
+		background: #FF9615
+	}
+	ul li{
+		list-style: none;
+		display: inline;
+		color:white;
+		margin-left:30px;
+		text-transform:uppercase;
+		font-size: 18px;
+	}
+	.NAVBARCOLOR{
+		background: BLACK;
+	}
+	.btn{
+		color:white;
+		border:none;
+		height: 35px;
+		width: 5%;
+		background-color: black;
+		margin-left: 3%;
+		margin-top: 3%;
+		border-radius: 5px;
+		font-size: 14px;
+	}
+	.btn: hover{
+		background-color: grey;
+		 box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+	}
+</style>
+<body>
+	<button class="btn">Back</button>
+	<button class="btn">Logout</button>
+	<center><h1>Online voting system </h1></center>>
+      <hr>
+    <NAV CLASS=NAVBARCOLOR><div> 
+        <ul>
+    		<li>Home</li>
+    		<li>login</li>
+    		<li>register</li>
+    	</ul>
+   	</div></NAV>
+
+</body>
+</html>
